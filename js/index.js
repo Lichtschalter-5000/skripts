b39a6265d69a5106511df1ce9fff918e9e9b6575
Lichtschalter <spitzbubenkoenig@web.de>
2018-12-29 17:03:47 +0100

    Caret and insert between two lines (#3)

--- js/index.js
+++ js/index.js
@@ -16,11 +16,12 @@ function setup(){
 	$("#0").find("input:first").addClass("caret");
 
 	
+	
 }
 
-function getNewRow(){
+function getNewRow(atIndex){
     var row = newrow;
-    row = row.replace("{INDEX}",index);
+    row = row.replace("{INDEX}",atIndex);
 
     var speaker = '<input type ="text">';
 
@@ -41,6 +42,10 @@ function attachHandlers() {
     $("td input").keydown(function(event){  
         switch(event.which){
             case 13://ENTER
+				
+				if(event.shiftKey) {
+					break;
+				} 
             case 9://TAB
                 event.preventDefault();
 
@@ -79,7 +84,30 @@ function attachHandlers() {
 
               
                 break;
-
+			
+			case 40://Arrow down
+				if(!event.ctrlKey){
+					return;
+				}
+				event.preventDefault();
+				$(this).closest("tr").prev("tr").addClass("caretBelow");
+				$(".caret").removeClass("caret");
+				$(this).blur();
+				
+			
+				break;
+				
+			case 38://Arrow up
+				if(!event.ctrlKey){
+					return;
+				}
+				event.preventDefault();
+				$(this).closest("tr").addClass("caretBelow");
+				$(".caret").removeClass("caret");
+				$(this).blur();
+				
+			
+				break;
         }
     });
 
@@ -88,6 +116,7 @@ function attachHandlers() {
 	$("td input").blur( function() {
 		$(this).parent().on("click", function(){
 			$(".caret").removeClass("caret");
+			$(".caretBelow").removeClass("caretBelow");
 			
             $(this).html('<input class = "caret" type ="text" value="'+$(this).text()+'">');
             $(this).children().select();
@@ -103,14 +132,39 @@ function attachHandlers() {
 		$(this).parent().html($(this).val());
 		attachHandlers();
 	});
+	
+	$(document).off("keydown");
+	
+	$(document).on("keydown", function(event) {
+		switch(event.which){
+			case 40://Arrow down
+				var oldtr = $(".caretBelow");
+				
+				if(oldtr.next("tr").length>0){
+				oldtr.removeClass("caretBelow");
+				oldtr.next("tr").addClass("caretBelow");
+				}
+				break;
+			case 38://Arrow up
+				var oldtr = $(".caretBelow");
+				
+				if(oldtr.prev("tr").length>0){
+				oldtr.removeClass("caretBelow");
+				oldtr.prev("tr").addClass("caretBelow");
+				}
+				break;
+			default: //any other key
+				var car = $(".caretBelow"); 
+				if(car.length>0) {
+					insertRow(parseInt(car.attr("id")));
+				}
+				car.removeClass("caretBelow");
+				break;
+		}
 		
-	/*$("td").on("click", function(){
-		console.log($(this).text());
-		$(this).html('<input type ="text" value="'+"hi"+'">');
-		$(this).children().select();
-		$(this).off("click");
-		attachHandlers();
-	});*/
+		
+	});
+	
 	
 }
 
@@ -118,13 +172,25 @@ function attachHandlers() {
 function insertRow(atIndex){
     if(atIndex === index ){
         //append:
-        $("#table").append(getNewRow());
+        $("#table").append(getNewRow(index));
         $("#table").find("tr:last").find("td input:first").focus();
 		$("#table").find("tr:last").find("td input:first").addClass("caret");
         index++;
     } else {
-        //ToDo insert somewhere
-        //console.log("atIndex!==index");
+        //insert somewhere else:
+		tr = $("tr.caretBelow#"+atIndex);
+		
+		tr.nextAll("tr").each(function() {
+			$(this).attr("id",parseInt($(this).attr("id"))+1);
+		});
+		
+		tr.after(getNewRow(atIndex+1));
+		index++;
+		
+		$("tr.caret td input:first").focus();
+		$("tr.caret td input:first").addClass("caret");
+		
+		
     }
     attachHandlers();
 }
